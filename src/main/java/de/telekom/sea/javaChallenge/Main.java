72199e5fd2b1f316f0e3c9e6efa2b459b1e89ab2
sea4 <f.leonhardt@telekom.de>
2021-06-02 09:52:11 +0200

    Part4

--- src/main/java/de/telekom/sea/javaChallenge/Main.java
+++ src/main/java/de/telekom/sea/javaChallenge/Main.java
@@ -3,21 +3,25 @@ package de.telekom.sea.javaChallenge;
 import de.telekom.sea.javaChallenge.part1.Part1;
 import de.telekom.sea.javaChallenge.part2.Part2;
 import de.telekom.sea.javaChallenge.part3.Part3;
+import de.telekom.sea.javaChallenge.part4.Part4;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		//Part1 part1 = new Part1();
-		//part1.run();
+		/*Part1 part1 = new Part1();
+		part1.run();
 		
-		//Part2 part2 = new Part2();
-		//part2.sortBySize();
+		Part2 part2 = new Part2();
+		part2.sortBySize();
 		
 		Part3 part3 = new Part3();
 		part3.runFor();
 		part3.runWhile();
-		part3.runDoWhile();
+		part3.runDoWhile(); */
+		
+		Part4 part4 = new Part4();
+		part4.count();
 	}
 
 }
---
+++ src/main/java/de/telekom/sea/javaChallenge/part4/Part4.java
@@ -0,0 +1,45 @@
+package de.telekom.sea.javaChallenge.part4;
+
+public class Part4 {
+	
+	private char[] letters = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	
+	private String poem = String.join( "", 
+	        "Whose woods these are I think I know.",
+	        "His house is in the village, though;",
+	        "He will not see me stopping here",
+	        "To watch his woods fill up with snow.",
+	        "",
+	        "My little horse must think it's queer",
+	        "To stop without a farmhouse near",
+	        "Between the woods and frozen lake",
+	        "The darkest evening of the year.",
+	        "",
+	        "He gives his harness bells a shake",
+	        "To ask if there's some mistake.",
+	        "The only other sound's the sweep",
+	        "Of easy wind and downy flake.",
+	        "",
+	        "The woods are lovely, dark, and deep,",
+	        "But I have promises to keep,",
+	        "And miles to go before I sleep,",
+	        "And miles to go before I sleep.");
+	
+	
+	public void count(String str) {
+		int i = 0;
+		for (char c : letters) {
+			i=0;
+			for (char v : str.toLowerCase().toCharArray()) {
+				if (c==v) {
+					i++;
+				}				
+			}
+			System.out.println("Der Buchstabe " + c + " kommt " + i + "mal vor");			
+		}
+	}
+	
+	public void count() {
+		count(poem);
+	}
+}
