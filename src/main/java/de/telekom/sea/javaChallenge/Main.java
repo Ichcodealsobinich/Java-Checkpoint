b5bea530a7021529ba7330bcc1bea23fa47bd35c
sea4 <f.leonhardt@telekom.de>
2021-06-02 10:46:35 +0200

    Part5

--- src/main/java/de/telekom/sea/javaChallenge/Main.java
+++ src/main/java/de/telekom/sea/javaChallenge/Main.java
@@ -4,6 +4,7 @@ import de.telekom.sea.javaChallenge.part1.Part1;
 import de.telekom.sea.javaChallenge.part2.Part2;
 import de.telekom.sea.javaChallenge.part3.Part3;
 import de.telekom.sea.javaChallenge.part4.Part4;
+import de.telekom.sea.javaChallenge.part5.*;
 
 public class Main {
 
@@ -18,10 +19,29 @@ public class Main {
 		Part3 part3 = new Part3();
 		part3.runFor();
 		part3.runWhile();
-		part3.runDoWhile(); */
+		part3.runDoWhile(); 
 		
 		Part4 part4 = new Part4();
-		part4.count();
+		part4.count();*/
+		
+		Part5 part5 = new Part5();
+		PersonImpl p1 = new PersonImpl();
+		p1.setVorname("Paul");
+		PersonImpl p2 = new PersonImpl();
+		p2.setVorname("Peter");
+		PersonImpl p3 = new PersonImpl();
+		p3.setVorname("Hans");
+		
+		
+		System.out.println("Ist die Liste leer? - " +part5.empty());
+		part5.add(p1);
+		System.out.println("Ist die Liste leer? - " +part5.empty());
+		part5.add(p2);
+		System.out.println("Ist die Liste leer? - " +part5.empty());
+		part5.add(p3);
+		System.out.println(part5.remove().getVorname() + " wurde entfernt");
+		System.out.println(part5.head().getVorname() + " steht vorne");
+		
 	}
 
 }
---
+++ src/main/java/de/telekom/sea/javaChallenge/part5/PersonImpl.java
@@ -0,0 +1,22 @@
+package de.telekom.sea.javaChallenge.part5;
+
+public class PersonImpl implements Person{
+	private String vorname;
+	private String nachname;
+	
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	
+	
+}
